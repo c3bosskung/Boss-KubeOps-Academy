2efa4e6ce83a066cfd709f70f81668b38d4f98d3
Kira <c3bosskung@gmail.com>
2022-09-09 03:18:08 +0700

    add js python java nginx(respone html hello world)

---
+++ asm-05-docker-web-app/1/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+http.createServer(
+    function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World');
+        console.log(req.rawHeaders)
+    })
+    .listen(8080, '0.0.0.0');
+
+console.log('Server running at http://0.0.0.0:8081/');
